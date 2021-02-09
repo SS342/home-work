4bd35564e12fbc466323f36866bc74fb349d37bc
SS342 <74982379+SS342@users.noreply.github.com>
2021-02-09 22:45:05 +0300

    Add files via upload

---
+++ hotel123.py
@@ -0,0 +1,10 @@
+revenue = int(input("выручка:"))
+days = int(input('кол-во дней:'))
+all_room = int(input('общее колво номеров:'))
+sell_room = int(input('колво проданых номеров:'))
+load = sell_room / (all_room * days)
+print(load * 100,'%')
+ADR = revenue / (all_room * days * (load / 100))
+print(ADR)
+RevPav = load / 100 * ADR
+print(RevPav)
